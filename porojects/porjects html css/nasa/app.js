fe25382313c779b44910d1378d3c8533196f6c1e
mehrdadj2005 <mehrdadjokari13@gmail.com>
2023-07-12 02:47:09 +0330

    اتمام پروژه ناسا
    برنامه نویس : مهرداد جوکاری
    زمان پروژه : 2 روز
    اولین پروژه شخصی خودم

---
+++ porojects/porjects html css/nasa/app.js
@@ -0,0 +1,6 @@
+let inputSearch = document.querySelector('#inputSearch')
+let searchHeader = document.querySelector('.search-header')
+
+inputSearch.addEventListener('click', () => {
+    searchHeader.style = `border:2px solid white;`
+})
