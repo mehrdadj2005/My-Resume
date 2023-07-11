f2663814934ea49b8c046d49a40fa1d93d4776be
mehrdadj2005 <mehrdadjokari13@gmail.com>
2023-07-12 00:20:33 +0330

    Initial commit

---
+++ porojects/porject javaScript/robat/app.js
@@ -0,0 +1,59 @@
+// function myFunction() {
+//     document.getElementById("form1").submit();
+//   }
+// let inputvalue = document.queryselector('#soall');
+
+
+
+
+
+// let x = "امتیازت چنده  ؟";
+
+// if (x.includes("درود")) {
+//     alert("درود شما");
+// } else if (x.includes("چند سالته ؟")) {
+//     alert("سالمه 20 ");
+// } else if (x.includes("امتیازت چنده ؟")) {
+//     alert(80);
+// }else{
+//     alert("مردود");
+// }
+// else if (soal.includes("")) {
+//     if (soal >= 50) {
+//         console.log("قبول");
+//     } else {
+//         console.log("رد");
+//     }
+// }
+
+
+
+
+let x = "درود";
+
+if (x.includes("رود") || x.includes("سلا")) {
+    console.log("سلام و درود بر شما ");
+} else if (x.includes("خوب") || x.includes("چطوری") || x.includes("khobi")) {
+    console.log("مرسی🌹 ، شما خوبین؟");
+} else if (x.includes("ممنون") || x.includes("مرسی") || x.includes("تشکر")) {
+    console.log("خوبه که خوبی");
+} else if (x.includes("نه")) {
+    console.log("برو دکتر😛😛");
+} else if (x.includes("امتیاز") || x.includes("چند")) {
+    console.log("80")
+} else if (x.includes("اسم") && x.includes("چی") || x.includes("چنن")) {
+    console.log("من مهرداد هستم و شما")
+} else if (x.includes("اسم") || x.includes("منم")) {
+    console.log("از اشنایی با شما خوشبختم💙💙💙")
+} else if (x.includes("بای") || x.includes("خداحافظ") || x.includes("خدا")) {
+    console.log("بدورد");
+} else if (x.includes("")) {
+    if (x >= 50) {
+        console.log("👍🏻");
+    } else {
+        console.log("👎🏻");
+    }
+}
+else{
+    console.log("متوجه منظورتون نمیشم!؟!؟!؟!؟!؟🧐🧐");
+}
