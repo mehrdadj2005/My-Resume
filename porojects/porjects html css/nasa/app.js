3e607ce3117369d2d4f7367049eeadeaf0584789
mehrdadj2005 <mehrdadjokari13@gmail.com>
2024-09-11 12:27:58 +0330

    # fix all project

--- porojects/porjects html css/nasa/app.js
+++
@@ -1,6 +0,0 @@
-let inputSearch = document.querySelector('#inputSearch')
-let searchHeader = document.querySelector('.search-header')
-
-inputSearch.addEventListener('click', () => {
-    searchHeader.style = `border:2px solid white;`
-})
